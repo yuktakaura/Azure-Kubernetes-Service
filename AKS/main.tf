d2d7b2e42fdfe562577723c89a3eea5e03d177ca
yuktakaura <160229261+yuktakaura@users.noreply.github.com>
2024-03-20 23:52:53 +0530

    Update main.tf

--- AKS/main.tf
+++ AKS/main.tf
@@ -1,8 +1,27 @@
 resource "azurerm_kubernetes_cluster" "example" {
-  name                = "example-aks1"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  dns_prefix          = "exampleaks1"
+  name                                = 
+  location                            = 
+  resource_group_name                 = 
+  dns_prefix                          = 
+  automatic_channel_upgrade           = var.automatic_channel_upgrade
+  azure_policy_enabled                = var.azure_policy_enabled
+  disk_encryption_set_id              = var.disk_encryption_set_id
+  dns_prefix                          = var.prefix
+  image_cleaner_enabled               = var.image_cleaner_enabled
+  image_cleaner_interval_hours        = var.image_cleaner_interval_hours
+  kubernetes_version                  = var.kubernetes_version
+  local_account_disabled              = var.local_account_disabled
+  node_os_channel_upgrade             = var.node_os_channel_upgrade
+  node_resource_group                 = var.node_resource_group
+  oidc_issuer_enabled                 = var.oidc_issuer_enabled
+  open_service_mesh_enabled           = var.open_service_mesh_enabled
+  private_cluster_enabled             = var.private_cluster_enabled
+  private_cluster_public_fqdn_enabled = var.private_cluster_public_fqdn_enabled
+  private_dns_zone_id                 = var.private_dns_zone_id
+  role_based_access_control_enabled   = var.role_based_access_control_enabled
+  run_command_enabled                 = var.run_command_enabled
+  sku_tier                            = var.sku_tier
+  support_plan                        = var.
 
   default_node_pool {
     name       = "default"
